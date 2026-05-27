64b32bc3fa24d32f290c04a525c3093531e2c27b
Elito Gomes <elito26br@gmail.com>
2026-05-26 21:50:33 -0300

    corrige tipo da edicao

--- app/page.tsx
+++ app/page.tsx
@@ -116,7 +116,7 @@ export default function AppFinanceiroPremiumAlpha() {
     setModalAberto(false);
   }
 
-  function editarConta(conta) {
+  function editarConta(conta: any) {
     setEditandoId(conta.id);
     setNovaConta({
       nome: conta.nome,
